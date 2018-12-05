c95dc62bbfec36f09784af018f2460b17c7c9cce
Anton Carter <antoncarter@fezmail.net>
2018-12-05 22:51:05 +0000

    inital commit

--- SerilogSinkFilter2_6/Program.cs
+++ SerilogSinkFilter2_6/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Serilog;
 
 namespace SerilogSinkFilter2_6
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
+            var testMessage = "do not show in log file";
+
+            //Async - This will filter the test message
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo
+                .Async(r => r.RollingFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "excluding.log"))
+                .Filter
+                    .ByExcluding(l => l.MessageTemplate.Text == testMessage))
+                .CreateLogger();
+
+            Log.Information("started logging");
+            Log.Information(testMessage);
+
+            Console.ReadKey(true);
         }
     }
 }
